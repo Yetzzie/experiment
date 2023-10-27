0a9f508bf090a0aeaf899637644ca635d6978230
Yetzzie <earljohnrequierme87@gmail.com>
2023-10-27 23:22:07 +0800

    ver1

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const myArry = [10,20,30,40,50];
 
+myArry.push(60,70,80,90);
+
 function multiply(a) {
-   return a * 2;
+   return a >= 50;
 };
 
- const paragraph = document.querySelector('.js-p');
- 
-console.log(myArry.map(multiply));
+ const paragraph = document.querySelector('.js-p').innerHTML = myArry.filter(multiply);
+
+console.log(myArry.filter(multiply));
+
+
+for(let i=1; i <= 5; i++) {
+  if(2 > i) {
+    console.log('youwin');
+  }
+
+  if(3 > i) {
+    console.log('YouWin2');
+  }
+}
+
