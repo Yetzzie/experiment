337aa8b787c676176acbaf27afef316a78e32fe3
Yetzzie <earljohnrequierme87@gmail.com>
2023-10-28 11:06:17 +0800

    ver2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 const myArry = [10,20,30,40,50];
 
 myArry.push(60,70,80,90);
@@ -20,4 +21,43 @@ for(let i=1; i <= 5; i++) {
     console.log('YouWin2');
   }
 }
+*/
+
+// Alternative
+ let chars = ['A', 'B', 'A', 'C', 'B', 'B', 'E'];
+
+ let uniqueChars = [...new Set(chars)];
+
+ console.log(uniqueChars);
+
+
+
+// this is if you want to remove duplicate in array
+let charss = ['A', 'B', 'A', 'C', 'B', 'D', 'D', 'E', 'E'];
+
+
+let uniqueCharss = charss.filter((c, index) => {
+    return charss.indexOf(c) === index;
+});
+
+console.log(uniqueCharss);
+
+console.log(charss.reverse());
+
+
+
+// This is interval
+let count = 0;
+ let setMy = setInterval(() => {  
+   count++;
+   console.log(count);
+
+   if(count === 10) {
+    clearInterval(setMy);
+   }
+ }, 1000);
+
+
+
+
 
