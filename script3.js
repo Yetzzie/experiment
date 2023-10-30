23ee710a1af35fd069a5d96b67d928bf22136bc6
Yetzzie <earljohnrequierme87@gmail.com>
2023-10-30 12:32:43 +0800

    ver5

--- script3.js
+++ script3.js
@@ -49,24 +49,64 @@ init().then(alert);
 
 
 
+/*
+async function loadJson(url) { // (1)
+  let response = await fetch(url); // (2)
 
-async function loadJson(url) {
-  try {
-    let response = await fetch(url);
-    if (response.status == 200) {
-      let yourLit = await response.json();
-      return yourLit;
-    } else {
-      throw new Error(response.status);
-    }
-  } catch (error) {
-    throw error;
+  if (response.status == 200) {
+    let json = await response.json(); // (3)
+    return json;
   }
+
+  throw new Error(response.status);
 }
 
 loadJson('https://javascript.info/no-such-user.json')
-  .then(alert)
-  .catch(alert);
+  .catch(alert); // Error: 404 (4)
+*/
+
+
+
+
+class HttpError extends Error {
+  constructor(response) {
+    super(`${response.status} for ${response.url}`);
+    this.name = 'HttpError';
+    this.response = response;
+  }
+}
+
+
+async function loadJson(url) {
+  let myHavoc = await fetch(url);
+    if(myHavoc.status == 200) {
+      let json = await myHavoc.json();
+      return json;
+    } else {
+      throw new HttpError(myHavoc);
+    }
+}
+
+// Ask for a user name until github returns a valid user
+async function demoGithubUser() {
+  let name = await prompt("Enter a name?", "iliakan");
+
+ let final = await loadJson(`https://api.github.com/users/${name}`).then((user) => {
+    alert(`Full name: ${user.name}.`);
+    return user;
+  }).catch((err) => {
+    if (err instanceof HttpError && err.response.status == 404) {
+      alert("No such user, please reenter.");
+      return demoGithubUser();
+    } else {
+      throw err;
+    };
+  });
+
+  return final;
+}
+
+demoGithubUser();
 
 
 
